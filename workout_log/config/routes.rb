617d8d8025918bcc9932a44dd7448da174457685
Tanaya Asnani <tasnani@umass.edugit config --global user.name Tanaya Asnani>
2018-05-28 17:19:07 -0400

    Added workout forms and views.

--- workout_log/config/routes.rb
+++ workout_log/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
 	resources :workouts
-	root 'workouts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+	root :to => 'workouts#index'
 end
